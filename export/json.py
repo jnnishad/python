9e38bc255a1cdf91b644e28c5e274bb1d122768a
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-02-10 15:57:18 +0530

    Delete json.py

--- export/json.py
+++
@@ -1,3 +0,0 @@
-{"paymentTransactionReq":
-     {"msgHdr":
-          {"msgId":"123","cnvId":"123","extRefId":"12345","bizObjId":"123455","appId":"ATOM","timestamp":"2019-05-08 11:43:37"},"msgBdy":{"paymentReq":{"custTxnRef":"ATM1557296017333","beneAccNo":"119878987895","beneName":"ATOMTEST","beneAddr1":"Hotel Skyway InnPremises No:21-22,","ifsc":"MUBL0000005","valueDate":"20190508","tranCcy":"INR","tranAmount":"1.36","purposeCode":"CMS","remitInfo1":"TowardsAPIBasedTesting","clientCode":"ATOM","paymentType":"IMPS","beneAccType":"SA","remarks":"SampleNEFTAPIBasedTxnat1509","delvMode":"","authPersonName":"","authPersonID":"","payLocation":"","routingCode":"","beneMail":"pritish.vartak@atomtech.in","beneMobile":"9819375136"}}}}
