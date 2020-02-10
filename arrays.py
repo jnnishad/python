b41cf99ffe29191e78c40f440f8a9bdd3b5fc423
jnnishad <50562744+jnnishad@users.noreply.github.com>
2020-02-10 16:16:07 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,27 +1,23 @@
-# import numpy
-#
-# arr=array([45,97,6],[154,5557,175])
-# print(arr)
+ import numpy
+
+ arr=array([45,97,6],[154,5557,175])
+ print(arr)
+
+ from array import *    #array build from user input
+ import sys
 
 
+ ip=['192.168.50.45','192.168.50.46']
+ for  i in ip:
+    print (i)
+ arr=array('i',[])
+ size=int(input('hi user how many numbers u want : '))
+ for i in range(size):
+     print (i+1, end=" ")
+     x=int(input("Enter ur value : "))
+     arr.append(x)
 
-
-# from array import *    #array build from user input
-# import sys
-#
-#
-# ip=['192.168.50.45','192.168.50.46']
-# for  i in ip:
-#    print (i)
-
-# arr=array('i',[])
-# size=int(input('hi user how many numbers u want : '))
-# for i in range(size):
-#     print (i+1, end=" ")
-#     x=int(input("Enter ur value : "))
-#     arr.append(x)
-#
-# print (arr)
+ print (arr)
 
 
 
